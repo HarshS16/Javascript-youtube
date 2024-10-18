bc61455a5efb3f42474723019419048ca3c8604d
HarshS16 <137524477+HarshS16@users.noreply.github.com>
2024-10-18 11:42:04 +0000

    first lecture

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const account_id = 1221
+let account_email="Harshme08@gmail.com"
+var account_password="#vk4india"
+account_city="Newdelhi"
+let accountstate
+console.table([account_email,account_id,account_password,accountstate,account_city]);
+/* Var is not recommended to use for variable declaration because of issue in functional scope and block scope
+const means the value of that quantity cannot be changed anytime in the future 
+let and var are used to declare variables, their values can be changed
+fourth method to declare and var is not recommended
+the best way to declare variables is let 
+console table is a good method to print multiple values in a tabular format
+if value of a variable is not declared js stores it as undefined
+ /*
