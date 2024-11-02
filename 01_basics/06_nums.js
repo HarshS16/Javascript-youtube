2c1c41d4d754f894fa24142515440bd56bb58bce
Harsh Srivastava <137524477+HarshS16@users.noreply.github.com>
2024-11-02 08:19:35 +0000

    .fixed, .string

--- 01_basics/06_nums.js
+++ 01_basics/06_nums.js
@@ -1,3 +1,8 @@
 const score =100
-const balance = new Number(100)
+const balance = new Number(100.3455)
 console.log(balance);
+console.log(balance.toString().length);
+//above statement converts the number to a string and prints the length of the string
+console.log(balance.toFixed(2));
+//above statement rounds off the number to 2 places after decimal
+
