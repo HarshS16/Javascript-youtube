0e416cefc74c854e048cdc7230b541f365acaf06
Harsh Srivastava <137524477+HarshS16@users.noreply.github.com>
2024-11-02 08:48:33 +0000

    to precision

--- 01_basics/06_nums.js
+++ 01_basics/06_nums.js
@@ -5,4 +5,7 @@ console.log(balance.toString().length);
 //above statement converts the number to a string and prints the length of the string
 console.log(balance.toFixed(2));
 //above statement rounds off the number to 2 places after decimal
+console.log(balance.toPrecision(4));
+//above statement rounds off the number to atmost 4 digits(4 most significant digits)
+
 
