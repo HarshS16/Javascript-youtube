5c3fead5ee196ca920a84b4d68f6d7598f01f82d
Harsh Srivastava <137524477+HarshS16@users.noreply.github.com>
2024-11-02 10:50:24 +0000

    push2

--- 01_basics/06_nums.js
+++ 01_basics/06_nums.js
@@ -26,5 +26,14 @@ console.log(Math.min(3,4,8,9,1));
 //returns minimum element of array
 console.log(Math.max(3,4,8,9,1));
 //returns maximum element of array
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+const min=10
+const max=20
+console.log(Math.floor(Math.));
+
+
+
 
 
