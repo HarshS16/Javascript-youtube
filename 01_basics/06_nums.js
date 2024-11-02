9cba743f0420cdfc6c257b33f99d573b5ec74a2b
Harsh Srivastava <137524477+HarshS16@users.noreply.github.com>
2024-11-02 09:23:24 +0000

    max min

--- 01_basics/06_nums.js
+++ 01_basics/06_nums.js
@@ -1,13 +1,30 @@
 const score =100
 const balance = new Number(100.3455)
-console.log(balance);
-console.log(balance.toString().length);
+//console.log(balance);
+//console.log(balance.toString().length);
 //above statement converts the number to a string and prints the length of the string
-console.log(balance.toFixed(2));
+//console.log(balance.toFixed(2));
 //above statement rounds off the number to 2 places after decimal
-console.log(balance.toPrecision(4));
+//console.log(balance.toPrecision(4));
 //above statement rounds off the number to atmost 4 digits(4 most significant digits)
 
 const hundreds= 100000000
-console.log(hundreds.toLocaleString('en-IN'));
+//console.log(hundreds.toLocaleString('en-IN'));
 //above statement adds commas to the large number, if bracket is empty it adds according to us standards
+
+//++++++++++++++++MATHS+++++++++++++++++++++
+//console.log(Math);
+//console.log(Math.abs(-8));
+//above returns absolute value(mod)
+//console.log(Math.round(6.5));
+//above rounds off the number to nearest integer
+//console.log(Math.ceil(6.05));
+//rounds off to nearest max value(6.05--->7)
+//console.log(Math.floor(6.95));
+//rounds off to nearest min value(6.95---->6)
+console.log(Math.min(3,4,8,9,1));
+//returns minimum element of array
+console.log(Math.max(3,4,8,9,1));
+//returns maximum element of array
+
+
