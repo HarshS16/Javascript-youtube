92e4e454e99ccbd7fdb2e3578c420b93cd2a06af
Harsh Srivastava <137524477+HarshS16@users.noreply.github.com>
2024-11-10 08:03:35 +0000

    ways to declare an array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,12 @@
+//arrays
+//arrays in js are resizable and can contain a mix of datatypes 
+// elements can be acessed through indexes and not values
+// ways to declare an array-->
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// how to print an element of the array-->
+console.log(myArr[1]);
+//prints second element(index1) of the array
