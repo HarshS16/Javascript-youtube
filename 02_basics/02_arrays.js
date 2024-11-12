d6648db021f07943a5b24cdb50fdc5d6e4652418
Harsh Srivastava <137524477+HarshS16@users.noreply.github.com>
2024-11-12 17:46:12 +0000

    push

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,2 +1,6 @@
 const marvel_heros=["ironman", "spider man","thor"];
 const dc_heros=["batman", "superman", "flash"];
+
+marvel_heros.push(dc_heros);
+// above statement adds the dc heros array into the marvel heros array as an array data type element
+console.log(marvel_heros);
