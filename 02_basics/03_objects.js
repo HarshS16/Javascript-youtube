5f9cbd9ff991fdeba95ca2f44bf8bcb36d574d59
Harsh Srivastava <137524477+HarshS16@users.noreply.github.com>
2024-11-21 17:55:33 +0000

    object added

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,7 +2,13 @@
 // object literals
 //object.create()   this is the constructor method
 there is another method used for declaring objects in JavaScript 
-const JsUser = {}
+const JsUser = {
+    name: "Harsh",
+    college:"JSS",
+    age: 20,
+    hobbies: ["reading", "coding"],
+    email="harshme08@gmail.com",
+}
 
 
 
