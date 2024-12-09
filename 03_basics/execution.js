fb9b1e8939735362777e40dcc3a7c62b6ea4d75e
Harsh Srivastava <harshme08@gmail.com>
2024-12-09 23:10:07 +0530

    execution context

---
+++ 03_basics/execution.js
@@ -0,0 +1,7 @@
+ // Javascript Execution Context
+ //1 GLobal Execution Context
+ //2 Function Execution Context
+ //3 Eval Execution Context
+
+// Global Execution Context
+// code is executed in 2 phases: memory creation phase and execution phase 
