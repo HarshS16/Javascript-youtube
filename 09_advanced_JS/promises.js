52ce8a50de08b6ce02aa8c7683a354f41f200a87
Harsh Srivastava <harshme08@gmail.com>
2025-01-14 23:52:23 +0530

    def added

--- 09_advanced_JS/promises.js
+++ 09_advanced_JS/promises.js
@@ -1,3 +1,7 @@
+//A Promise is a proxy for a value not necessarily known when the promise is created. It allows you to associate handlers with an asynchronous action's eventual success value or failure reason. This lets asynchronous methods return values like synchronous methods: instead of immediately returning the final value, the asynchronous method returns a promise to supply the value at some point in the future.
+
+
+
 const promiseOne = new Promise(function(resolve, reject){
     //Do an async task
     // DB calls, cryptography, network
